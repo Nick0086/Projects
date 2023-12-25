8ae620ceb2767b7e1a269efde03af8ce6a9a8eaa
Nick0086 <nikunjkalathiya86@gmail.com>
2023-12-25 12:06:09 +0530

    04counter project

---
+++ 04counter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+function App() {
+
+  const [counter,setCounter] = useState(0);
+
+
+  return (
+    <>
+      <button onClick={(e) => setCounter(counter - 1)}>-</button>
+      <h1>Counter: {counter}</h1>
+      <button onClick={(e) => setCounter(counter + 1)} >+</button>
+    </>
+  );
+}
+
+export default App;
