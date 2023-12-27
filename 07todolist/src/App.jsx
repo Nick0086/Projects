9013602f5f2fc3930e4550d8d7c7b30006c345b7
Nick0086 <nikunjkalathiya86@gmail.com>
2023-12-27 19:14:56 +0530

    project 07 TODOList complate

--- 07todolist/src/App.jsx
+++ 07todolist/src/App.jsx
@@ -1,46 +1,56 @@
 import './App.css';
 import TodoHeader from './components/TodoHeader';
 import { FaSquarePlus } from "react-icons/fa6";
+
+
 import TodoList from './components/TodoList';
-import { useState ,useId} from 'react';
+import { useState, useId } from 'react';
 
 function App() {
 
-  const [todos , setTodos] = useState([]);
-  const [task , setTask] = useState('');
-  const [id , setid] = useState(0)
-  
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+  const [id, setid] = useState(0)
+
 
   const todoHandler = (e) => {
-    e.preventDefault(); 
-    setTodos( (prev) => [...prev , {task,id}]);
-    console.log(todos)
+    e.preventDefault();
+    setTodos((prev) => [...prev, { task, id }]);
     setTask('');
     setid(id + 1);
   }
 
-  const deleteTodo = (eId) =>{
-    const arry = todos.filter((todo) => todo.id !== eId );
+  const deleteTodo = (eId) => {
+    const arry = todos.filter((todo) => todo.id !== eId);
     setTodos(arry);
   }
+
+  const newTask = (nt, eId) => {
+    const arry = todos.map((todo) => {
+      return todo.id == eId ? { ...todo, task: nt } : todo;
+    })
+    setTodos(arry)
+  };
 
 
   return (
     <>
       <div className='todo-body'>
-        <TodoHeader/>
+        <TodoHeader />
         <div className='todo-input-body' >
-            <form className='todo-form' onSubmit={todoHandler} >
-              <input type="text" className='task-input' placeholder="Add a new Task..." name="todo" value={task} onChange={(e) => setTask(e.target.value)} />
-              <button type='submit' className='add-btn'><FaSquarePlus className='plus-icon' /></button>
-            </form>
+          <form className='todo-form' onSubmit={todoHandler} >
+            <input type="text" className='task-input' placeholder="Add a new Task..." name="todo" value={task} onChange={(e) => setTask(e.target.value)} />
+            <button type='submit' className='add-btn'><FaSquarePlus className='plus-icon' /></button>
+          </form>
         </div>
-        {
-          todos.length !== 0 &&
-          todos.map((todo) => {
-            return <TodoList key={todo.id} {...todo} deleteTodo={deleteTodo}/>
-          })
-        }
+        <ul className='ul' >
+          {
+            todos.length !== 0 &&
+            todos.map((todo) => {
+              return <TodoList key={todo.id} {...todo} deleteTodo={deleteTodo} newTask={newTask} />
+            })
+          }
+        </ul>
       </div>
     </>
   );
--- 07todolist/src/components/TodoList.jsx
+++ 07todolist/src/components/TodoList.jsx
@@ -1,16 +1,33 @@
 import React, { useState } from 'react'
 import { MdDeleteForever } from "react-icons/md";
+import { FaEdit, FaSave } from "react-icons/fa";
 
-function TodoList({task,id,deleteTodo}) {
+function TodoList({ task, id, deleteTodo, newTask }) {
 
-    const[check , setCheck] = useState(false)
+    const [check, setCheck] = useState(false);
+    const [edit, setEdit] = useState(false);
+    const [newvalue, setNewvalue] = useState(task)
+
+    const editHandler = () => {
+        if (!edit) {
+            setEdit(!edit)
+        } else {
+            newTask(newvalue , id);
+            setEdit(!edit)
+        }
+    }
 
     return (
         <>
             <li className='flex'>
-                <input  type="checkbox" className='custom-checkbox' onClick={() => setCheck(!check)} />
-                <input id='id' type="text" className={`todo-name ${check ? "line" : ''} `} value={task} readOnly />
-                <MdDeleteForever className='remove-icon' onClick={(e) => deleteTodo(id)} />
+                <input type="checkbox" className='custom-checkbox' onClick={() => setCheck(!check)} />
+                <input id='id' type="text" className={`todo-name ${check ? "line" : ''} ${edit ? "task-input" : ''} `} value={newvalue} readOnly={!edit} onChange={(e) => setNewvalue(e.target.value)} />
+                <div className='remove-icon' onClick={editHandler} >
+                    {
+                        !edit ? <FaEdit /> : <FaSave/>
+                    }
+                </div>
+                <MdDeleteForever className='remove-icon edit' onClick={(e) => deleteTodo(id)} />
             </li>
         </>
     )
