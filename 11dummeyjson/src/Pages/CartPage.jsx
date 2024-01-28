3ee9775596cd79e4d2e1af26b225fafdf9a87a4b
Nick0086 <nikunjkalathiya86@gmail.com>
2024-01-28 11:36:19 +0530

    change

--- 11dummeyjson/src/Pages/CartPage.jsx
+++ 11dummeyjson/src/Pages/CartPage.jsx
@@ -6,6 +6,7 @@ import ProductBill from '../Components/ProductBill';
 function CartPage() {
     return (
         <Container>
+        <h2 className='text-3xl text-center lg:my-10 md:my-6 my-4 font-bold' >Your Cart</h2>
         <div className='grid grid-cols-12 md:gap-8 gap-y-4' >
             <div className='md:col-span-9 col-span-12 '>
                 <CartProducts />
